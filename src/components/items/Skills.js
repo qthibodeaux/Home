10c71db66ca5eba189a65055f85bad7e7e20f0bf
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2023-01-02 19:10:51 -0600

    Updates to skill section

--- src/components/items/Skills.js
+++ src/components/items/Skills.js
@@ -1,5 +1,7 @@
-import { Box, Heading, Button } from 'grommet'
+import { useContext } from 'react'
+import { Box, Button, Heading, ResponsiveContext } from 'grommet'
 import { Code } from 'grommet-icons'
+
 /*
 <a href="https://www.flaticon.com/free-icons/node-js" title="node js icons">Node js icons created by Freepik - Flaticon</a>
 <a href="https://www.flaticon.com/free-icons/html5" title="html5 icons">Html5 icons created by Freepik - Flaticon</a>
@@ -14,8 +16,7 @@ import { Code } from 'grommet-icons'
 
 */
 
-
-function Skills () {
+function SkillSmall () {
     return (
         <Box 
             background='dark-1' 
@@ -91,6 +92,112 @@ function Skills () {
                 </Box>
             </Box>
         </Box>
+    )
+}
+
+function SkillBig () {
+    return (
+        <Box 
+            background='dark-1' 
+            fill='horizontal' 
+            pad='large' 
+            justify='center'
+            align='center'
+            gap='large'
+        >
+            <Heading level='4' margin='none'>My Experience</Heading>
+            <Box
+                width='large'
+                direction='row'
+                justify='between'
+            >
+                <Box 
+                    pad='large'
+                    justify='center'
+                    align='between'
+                    background='dark-2'
+                    border={{ color: 'light-1', size: 'medium' }}
+                    round={{ size: 'medium' }}
+                    gap='medium'
+                    elevation="medium"
+                >
+                    <Heading textAlign='center' level='6' margin='none'>Frontend Development</Heading>
+                    <Box 
+                        direction='row'
+                    >
+                        <Box 
+                            pad='small'
+                            justify='start'
+                            align='start'
+                            gap='medium'
+                        >
+                            <Button plain icon={<Code />} label='HTML' />
+                            <Button plain icon={<Code />} label='Javascript' />
+                            <Button plain icon={<Code />} label='CSS' />
+                        </Box>
+                        <Box 
+                            pad='small'
+                            justify='start'
+                            align='start'
+                            gap='medium'
+                        >
+                            <Button plain icon={<Code />} label='React' />
+                            <Button plain icon={<Code />} label='Redux' />
+                            <Button plain icon={<Code />} label='Recoil' />
+                        </Box>
+                    </Box>
+                </Box>
+                <Box
+                    pad='medium'
+                    justify='center'
+                    align='between'
+                    background='dark-2'
+                    border={{ color: 'light-1', size: 'medium' }}
+                    round={{ size: 'medium' }}
+                    gap='medium'
+                    elevation="medium"
+                >
+                    <Heading textAlign='center' level='6' margin='none'>Backend Development</Heading>
+                    <Box 
+                        direction='row'
+                    >
+                        <Box 
+                            pad='small'
+                            justify='start'
+                            align='start'
+                            gap='medium'
+                        >
+                            <Button plain icon={<Code />} label='Node JS' />
+                            <Button plain icon={<Code />} label='Postgres' />
+                            <Button plain icon={<Code />} label='Sequelize' />
+                        </Box>
+                        <Box 
+                            pad='small'
+                            justify='start'
+                            align='start'
+                            gap='medium'
+                        >
+                            <Button plain icon={<Code />} label='MongoDB' />
+                            <Button plain icon={<Code />} label='Knex' />
+                            <Button plain icon={<Code />} label='Twilio' />
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+
+function Skills () {
+    const size = useContext(ResponsiveContext)
+    return (
+        <div>
+            {size !== 'small'
+                ? <SkillBig />
+                : <SkillSmall />
+            }
+        </div>
     )
 }
 
